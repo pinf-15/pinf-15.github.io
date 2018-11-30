795f63d24ca9118f72994932af6cf66e7d5ab1be
PlasticShitlord <amatiel98@mail.ru>
2018-11-30 11:47:45 +0300

    calculator comments

--- js/price_calculator.js
+++ js/price_calculator.js
@@ -1,5 +1,13 @@
+// Список языков, с которых переводим. Вставляем value в кавычки
 var lang_from = new Array
 lang_from["None"]=0
+lang_from["100"]=100
+lang_from[""]=
+lang_from[""]=
 
+// На какие языки переводим
 var lang_to = new Array
 lang_to["None"]=0
+lang_from["100"]=100
+lang_from[""]=
+lang_from[""]=
