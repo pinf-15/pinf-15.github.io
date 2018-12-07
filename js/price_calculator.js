159c5f95db6fb22cf9167a4791807d13baf669a4
PlasticShitlord <amatiel98@mail.ru>
2018-12-07 11:23:37 +0300

    Update price_calculator.js

--- js/price_calculator.js
+++ js/price_calculator.js
@@ -4,30 +4,28 @@ lang_from_prices["None"]=0
 lang_from_prices["German"]=20;
 lang_from_prices["Norwegian"]=25;
 lang_from_prices["Polish"]=35;
-lang_from_prices["Russian"]=50
-lang_from_prices["English"]=55
-
+lang_from_prices["Russian"]=50;
+lang_from_prices["English"]=55;
 
 var lang_to_prices = new Array();
 lang_to_prices["None"]=0;
 lang_to_prices["German"]=20;
 lang_to_prices["Norwegian"]=25;
 lang_to_prices["Polish"]=25;
-lang_to_prices["Russian"]=50
-lang_to_prices["English"]=55
+lang_to_prices["Russian"]=50;
+lang_to_prices["English"]=55;
 
 var service_type_prices = new Array();
-service_type_prices["None"]=0
-service_type_prices["Fictional"]=1.5
-service_type_prices["Economical"]=1.1
-service_type_prices["Technical"]=1.2
-service_type_prices["Medicine"]=1.4
-service_type_prices["Publicistic"]=1
+service_type_prices["None"]=0;
+service_type_prices["Fictional"]=1.5;
+service_type_prices["Economical"]=1.1;
+service_type_prices["Technical"]=1.2;
+service_type_prices["Medicine"]=1.4;
+service_type_prices["Publicistic"]=1;
 
-var words = new Array();
-document.getElementById("words").value || 0
-
+// Берем данные из формы
 var theForm = document.forms["langform"];
+
 
 
 function getLangFromPrice()
@@ -63,6 +61,7 @@ function calculateTotal()
    result_price.innerHTML = "Total Price $"+sumPrice;
 }
 
+// спрятать результат
 function hideTotal()
 {
    var result_price = document.getElementById('totalPrice');
